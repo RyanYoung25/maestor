375aa41f41f0b9ff80242fabe2b398068828503f
Ryan Young <Ryan@rdyoung.us>
2014-04-08 13:10:04 -0400

    Added some install scripts and a script that generates a console api

---
+++ run/sim-real.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $(pgrep hubo-daemon) ]; then
+    echo "Hubo-Ach daemon is already running. Terminating..."
+    hubo-ach killall &> /dev/null
+    echo "Hubo-Ach daemon terminated."
+fi
+
+xterm -e "roscore" &
+
+export PYTHONPATH="$PYTHONPATH:/usr/lib/python2.7/dist-packages" 
+xterm -e "hubo-ach sim openhubo nophysics simtime" &
+
+while [ ! $(pgrep hubo-daemon) ]; do
+    sleep 1
+done
+
+sudo -E ~/git/git/maestor/bin/maestor
+
+while true; do
+    sleep 3
+done
