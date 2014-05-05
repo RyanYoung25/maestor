ad38099aaae7861066a2ef561e34b113af425b74
hubo <hubo@hubo.dhcp.drexel.edu>
2014-05-05 11:48:30 -0400

    I changed testure.

--- scripts/testure.py
+++ scripts/testure.py
@@ -5,12 +5,12 @@ robot = maestor()
 #initialization and enabling. 
 #skipping the initialization because homing is currently not perfect. 
 #The testure gesture in python
-robot.setProperties("RSR RSR", "velocity position", "1.0 -1.35")
+robot.setProperties("RSR RSR", "velocity position", ".50 -1.35")
 robot.waitForJoint("RSR")
 
-robot.setProperties("RSY RSY", "velocity position", "1.0 1.57")
+robot.setProperties("RSY RSY", "velocity position", ".50 1.57")
 robot.waitForJoint("RSY")
-robot.setProperty("REP", "velocity", 1.0)
+robot.setProperty("REP", "velocity", .5)
 
 for i in range(0, 5):
     robot.setProperty("REP", "position", -1.7)
@@ -18,7 +18,7 @@ for i in range(0, 5):
     robot.setProperty("REP", "position", 0)
     robot.waitForJoint("REP")
 
-robot.setProperties("RSY RSY", "velocity position", "1.0 -1.57")
+robot.setProperties("RSY RSY", "velocity position", ".5 -1.57")
 robot.waitForJoint("RSY")
 
 for i in range(0, 5):
