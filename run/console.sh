278347e72024a768856d0eb9ca7c6ef708f97099
Ryan Young <ryan@rdyoung.us>
2017-03-01 13:53:29 -0500

    Sim script appears to work

---
+++ run/console.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+cd /opt/maestor/console/
+./console.py
+python -i ./maestorapi.py 
--- setupMaestor.sh
+++ setupMaestor.sh
@@ -1,5 +1,14 @@
 MAESTOR_DIR=$(pwd)
 sudo mkdir -p /opt/maestor/
 sudo mkdir -p /opt/maestor/logs/
+
+mkdir -p ./bin
+
+ln -s $MAESTOR_DIR/../../devel/lib/maestor/maestor_node ./bin/maestor_node
+
 sudo ln -s $MAESTOR_DIR/config /opt/maestor/config
 sudo ln -s $MAESTOR_DIR/models /opt/maestor/models
+sudo ln -s $MAESTOR_DIR/console /opt/maestor/console
+sudo ln -s $MAESTOR_DIR/run /opt/maestor/run
+sudo ln -s $MAESTOR_DIR/bin /opt/maestor/bin
+
